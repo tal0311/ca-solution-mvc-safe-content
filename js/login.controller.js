870d2aeda4c9044efa58ab165f964ca68f678263
Tal Amit <83287794+tal0311@users.noreply.github.com>
2022-10-08 19:39:32 +0300

    user msg

--- js/login.controller.js
+++ js/login.controller.js
@@ -1,1 +1,42 @@
-console.log("login controller");
+"use strict";
+
+function loginInit() {
+  console.log("%c Login Init", "color:lightgreen");
+  createUsers();
+  renderLogin();
+}
+
+function renderLogin() {
+  const loggedInUser = getLoggedInUser();
+  console.log(loggedInUser);
+
+  const elLogin = document.querySelector(".login");
+  const elSecret = document.querySelector(".secret-content");
+  if (!loggedInUser) {
+    renderLoggedIdUser(elLogin, elSecret);
+  }
+}
+
+function renderLoggedIdUser(elLogin, elSecret) {
+  elLogin.classList.remove("hide");
+  elSecret.classList.add("hide");
+}
+
+function onLogin(ev) {
+  ev.preventDefault();
+  const formData = new FormData(ev.target);
+  const inputObject = Object.fromEntries(formData);
+  // convert the FormData object to a normal object
+  const { username, passWord } = inputObject;
+  if (!passWord && !username) {
+    setUserMsg("Password or username are not correct");
+    return;
+  }
+}
+
+function setUserMsg(msg) {
+  document.querySelector(".user-msg").classList.toggle("open");
+  if (!msg) return;
+  document.querySelector(".msg-txt").innerText = msg || "";
+  setTimeout(setUserMsg, 2000);
+}
--- js/login.service.js
+++ js/login.service.js
@@ -1,1 +1,37 @@
-console.log("login service");
+"use strict";
+const KEY = "usersDB";
+var gLoggedInUser = null;
+
+function getLoggedInUser() {
+  return gLoggedInUser;
+}
+
+function createUsers() {
+  const users = loadFromStorage(KEY) || [];
+  if (!users || !users.length) {
+    users.push(
+      _createUser("admin", 1),
+      _createUser("user2", 2),
+      _createUser("user1", 3)
+    );
+  }
+}
+
+function _createUser(username, password, isAdmin = false) {
+  return {
+    id: _makeId(),
+    username,
+    password,
+    isAdmin,
+    lastLoginAt: Date.now()
+  };
+}
+
+function _makeId(length = 5) {
+  var txt = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < length; i++) {
+    txt += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return txt;
+}
